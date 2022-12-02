7e7dce17ea5f52b2440b0940c34437abd8391de0
Victor <victorgrubiodl@gmail.com>
2022-12-02 18:40:27 +0100

    Removed unused template

--- scaffolder-templates/springboot-grpc-template/skeleton/src/main/java/${{values.java_package_name}}/Application.java
+++ scaffolder-templates/springboot-grpc-template/skeleton/src/main/java/${{values.java_package_name}}/Application.java
@@ -1,4 +1,4 @@
-package com.example.${{values.java_package_name}};
+package ${{values.java_package_name}};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
